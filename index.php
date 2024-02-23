516e46d86ed02358734e8296dfeb3d33cef444e2
Manoel Carvalho <manoelmecat@gmail.com>
2024-02-23 06:57:29 -0300

    feat: initial commit

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Point do Açaí</title>
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Protest+Revolution&family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+</head>
+<body>
+  <div>
+    <img src="images/logo.png" alt="logo">
+    <main>
+      <div class="borda esquerda">
+        <form action="">
+          <label for="Valor do peso">Valor do peso</label>
+          <input type="number" name="peso">
+
+          <label for="peso">Peso</label>
+          <input type="number" name="peso">
+
+          <label for="Valor a ser pago">Valor a ser pago</label>
+          <input type="number" name="valor">
+          <button>Calcular</button>
+        </form>  
+
+      </div>
+      <div class="borda direita">
+        <p style="text-align: center; font-size: 2rem;">Caixa</p>
+        22/02/2024 - 20,00<br>
+        22/02/2024 - 20,00<br>
+        22/02/2024 - 20,00<br>
+        22/02/2024 - 20,00<br>
+        22/02/2024 - 20,00<br>
+        22/02/2024 - 20,00<br>
+        22/02/2024 - 20,00<br>
+        22/02/2024 - 20,00<br>
+      </div>
+    </main>
+  </div>
+</body>
+</html>
